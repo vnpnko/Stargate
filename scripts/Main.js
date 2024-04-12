05f133e520263c4558d1c07f20efa2bedd1dc514
RF9R9C <rf9r9c@inf.elte.hu>
2024-04-12 23:09:25 +0200

    create Map class

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,14 +1,17 @@
 import { Player } from './Player.js';
 import { OasisMarker } from './OasisMarker.js';
 import { Sand } from './Sand.js';
-import {Oasis} from "./Oasis.js";
-import {Drought} from "./Drought.js";
-import {Item} from "./Item.js";
+import { Oasis } from "./Oasis.js";
+import { Drought } from "./Drought.js";
+import { Item } from "./Item.js";
+import { createLevel, updateMap } from "./Map.js";
 
 const matrix = document.getElementById('matrix');
 
+let numPlayers;
+let players = [];
+
 window.onload = function() {
-    let numPlayers;
     const setNumPlayersForm = document.getElementById('setNumPlayers');
     const setNamePlayersForm = document.getElementById('setNamePlayers');
 
@@ -19,7 +22,7 @@ window.onload = function() {
 
         for (let i = 1; i <= numPlayers; i++) {
             const label = document.createElement('label');
-            label.for = `player_${i}-name`;
+            label.htmlFor = `player_${i}-name`;
             label.textContent = `Player ${i} name: `;
             label.classList.add('text-2xl', 'm-auto');
 
@@ -50,129 +53,52 @@ window.onload = function() {
 
         setNamePlayersForm.style.display = 'none';
         document.getElementById('startModal').style.display = 'none';
+
     });
 }
 
-function getRandomSet(length) {
-    let set = new Set();
-    while (set.size < length) {
-        let i = Math.floor(Math.random() * 5);
-        let j = Math.floor(Math.random() * 5);
+const currentMatrix = createLevel(players);
 
-        if (set.has(`${i},${j}`) || (i === 2 && j === 2)) {
-            continue;
-        }
-        set.add(`${i},${j}`);
-    }
-    return set;
-}
-
-let locations = getRandomSet(7);
-
-
-let player;
-
-function createLevel() {
-    let hasDrought = false;
-    let cells = [];
-    for (let i = 0; i < 5; i++) {
-        let row = [];
-        for (let j = 0; j < 5; j++) {
-            let cell;
-            if (i === 2 && j === 2) {
-                cell = new Player(i, j);
-                player = new Player(i, j, new Sand(i, j));
-            } else if (locations.has(`${i},${j}`)) {
-                if (locations.size > 3) {
-                    if (hasDrought === false) {
-                        cell = new OasisMarker(i, j, 'Drought');
-                        hasDrought = true;
-                    } else {
-                        cell = new OasisMarker(i, j, 'Oasis');
-                    }
-                } else {
-                    cell = new Item(i, j, locations.size);
-                }
-                locations.delete(`${i},${j}`);
-            } else {
-                cell = new Sand(i, j);
-            }
-            row.push(cell);
-        }
-        cells.push(row);
-    }
-    return cells;
-}
-
-const currentMatrix = createLevel();
-
-function updateMap(cells) {
-    cells.forEach(row => {
-        row.forEach(cell => {
-            const div = document.createElement('div');
-            div.classList.add('cell', 'w-20', 'h-20', 'bg-yellow-200');
-
-            if (cell instanceof OasisMarker) {
-                div.id = `OasisMarker/${cell.markerType}`;
-            } else {
-                div.id = cell.constructor.name;
-            }
-
-            if (cell instanceof Sand) {
-                div.classList.add(cell.bgColor);
-            } else if (cell instanceof Item && cell.hidden === true) {
-                div.classList.add('bg-yellow-300');
-            } else {
-                div.style.backgroundImage = cell.bgImage;
-            }
-
-
-            div.style.backgroundSize = 'cover';
-            matrix.appendChild(div);
-        });
-    });
-}
-
-updateMap(currentMatrix);
+updateMap(currentMatrix, matrix);
 
 window.addEventListener('keydown', function (event) {
     const key = event.key;
     const validKeys = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
 
-    if (validKeys.includes(key) && numPlayers > 0) {
-        const oldCell = currentMatrix[player.x][player.y];
-        const oldLoc = player.loc;
+    if (validKeys.includes(key)) {
+        const oldCell = currentMatrix[players[0].x][players[0].y];
+        const oldLoc = players[0].loc;
 
-        if (key === 'ArrowLeft' && player.y > 0) {
-            player.moveLeft();
-        } else if (key === 'ArrowRight' && player.y < 4) {
-            player.moveRight();
-        } else if (key === 'ArrowUp' && player.x > 0) {
-            player.moveUp();
-        } else if (key === 'ArrowDown' && player.x < 4) {
-            player.moveDown();
+        if (key === 'ArrowLeft' && players[0].y > 0) {
+            players[0].moveLeft();
+        } else if (key === 'ArrowRight' && players[0].y < 4) {
+            players[0].moveRight();
+        } else if (key === 'ArrowUp' && players[0].x > 0) {
+            players[0].moveUp();
+        } else if (key === 'ArrowDown' && players[0].x < 4) {
+            players[0].moveDown();
         } else {
             return;
         }
 
-        player.loc = currentMatrix[player.x][player.y];
-        currentMatrix[player.x][player.y] = new Player(player.x, player.y, player.loc);
+        players[0].loc = currentMatrix[players[0].x][players[0].y];
+        currentMatrix[players[0].x][players[0].y] = new Player(players[0].x, players[0].y, players[0].loc, players[0].name);
         currentMatrix[oldCell.x][oldCell.y] = oldLoc;
 
         matrix.innerHTML = '';
-        updateMap(currentMatrix);
+        updateMap(currentMatrix, matrix);
     }
 
     if (key === ' ') {
-        if (player.loc instanceof OasisMarker) {
-            if (player.loc.markerType === 'Oasis') {
-                player.loc = new Oasis(player.x, player.y);
+        if (players.at(0).loc instanceof OasisMarker) {
+            if (players.at(0).loc.markerType === 'Oasis') {
+                players.at(0).loc = new Oasis(players.at(0).x, players.at(0).y);
             } else {
-                player.loc = new Drought(player.x, player.y);
+                players.at(0).loc = new Drought(players.at(0).x, players.at(0).y);
             }
         }
-        if (player.loc instanceof Item && player.loc.hidden === true) {
-            player.loc.hidden = false;
+        if (players.at(0).loc instanceof Item && players.at(0).loc.hidden === true) {
+            players.at(0).loc.hidden = false;
         }
     }
 });
